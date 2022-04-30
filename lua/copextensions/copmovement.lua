e98faf725f2abeb1457e236f13d44744f049bc0b
Fuglore <44823551+fuglore@users.noreply.github.com>
2022-04-30 11:08:53 -0300

    autoupdates

--- lua/copextensions/copmovement.lua
+++
@@ -1,7 +0,0 @@
-Hooks:PostHook(CopMovement, "_upd_actions", "lies_actions", function(self, t)
-	if not self._need_upd then
-		local ext_anim = self._ext_anim
-		
-		self._need_upd = ext_anim.fumble and true or false
-	end
-end)

fbb9a8b60a0c64fa26beb2b7766e6ee4e2daa5f8
Fuglore <44823551+fuglore@users.noreply.github.com>
2022-06-19 03:04:46 -0300

    Update menumanager.lua

--- menumanager.lua
+++ menumanager.lua
@@ -5,7 +5,7 @@ if RequiredScript == "lib/managers/menumanager" then
 		save_path = SavePath .. "LittleIntelligenceEnhancementS.txt",
 		default_loc_path = ModPath .. "loc/en.txt",
 		options_path = ModPath .. "menu/options.txt",
-		version = "V3.47",
+		version = "V3.48",
 		settings = {
 			lua_cover = false,
 			jokerhurts = false,
